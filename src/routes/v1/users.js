63a8db35078f8ecce7fe00b053e88b3be11ce3b4
AndryGamerTV <asdaartefret@gmail.com>
2025-02-18 19:14:24 +0200

    users update error add

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -6,8 +6,9 @@ const router = express.Router();
 
 router.get('/', isAuthorized, function(req, res){
       Models.User.findAll().then((users) => {
-            res.json(users);//ліміт
-      }).catch;// gjvbkrf
+            res.json(users); //ліміт
+            
+      }).catch(()=> res.status(500).json({ message: 'Fail to find Users' })); // помилка
 });
 
 export const users = router;
