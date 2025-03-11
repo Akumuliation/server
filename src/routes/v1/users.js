d7d2549d2c0b2ec6b2fd5e3de93ae8342f36528b
AndryGamerTV <asdaartefret@gmail.com>
2025-03-11 22:50:02 +0200

    upd

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { isAprroved, isAuthorized } from '../../helpers/index.js';
+import { app, isAprroved, isAuthorized } from '../../helpers/index.js';
 import Models from '../../models/index.js';
 
 const router = express.Router();
@@ -9,6 +9,22 @@ router.get('/', isAuthorized, isAprroved, function(req, res){
       Models.User.findAndCountAll({ limit, approved, offset }).then(({ count, rows: users }) => {
             res.setHeader("x-total-count", count).json(users); //ліміт
       }).catch(()=> res.status(500).json({ message: 'Fail to find Users' })); // помилка
-});
+      }
+);
 // зробити start і end для users
 export const users = router;
+
+
+
+
+/*                setTimeout(() => {
+                  const newUsers = users.concat(Array.from({ length: 20 }));
+                  users.push(...newUsers);
+                  limit++;
+                  approved = true;
+            }, 1000);
+            window.addEventListener('scroll', () => {
+                  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+                  if (scrollTop + clientHeight >= scrollHeight - 5 && approved) {
+                        fetchMoreData();
+                  }*/
