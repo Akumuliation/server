eb38e944e47b58a518c2918e4b0634ad1554c0a0
AndryGamerTV <asdaartefret@gmail.com>
2025-03-13 23:07:47 +0200

    list users upd

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 router.get('/', isAuthorized, isAprroved, function(req, res){
       const { approved = true, limit = 20, offset = 0 } = req.query;
-      Models.User.findAndCountAll({ limit, approved, offset }).then(({ count, rows: users }) => {
+      Models.User.findAndCountAll({ 
+            limit: parseInt (limit, 10),
+            offset: parseInt (offset, 10),
+            where: { approved }
+      }).then(({ count, rows: users }) => {
             res.setHeader("x-total-count", count).json(users); //ліміт
       }).catch(()=> res.status(500).json({ message: 'Fail to find Users' })); // помилка
       }
