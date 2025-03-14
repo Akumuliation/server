3fb8a1c493d7c1190c9ffb580281ad5d07d958a7
AndryGamerTV <asdaartefret@gmail.com>
2025-03-14 12:42:56 +0200

    start_end=>limit_offset

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -4,15 +4,18 @@ import Models from '../../models/index.js';
 
 const router = express.Router();
 
+function convertStartEndToLimitOffset (start, end){
+      const limit = end - start;
+      const offset = start;
+      return { limit, offset };
+}
+
 router.get('/', isAuthorized, isAprroved, function(req, res){
-      const { approved = true, limit = 20, offset = 0 } = req.query;
-      Models.User.findAndCountAll({ 
-            limit: parseInt (limit, 10),
-            offset: parseInt (offset, 10),
-            where: { approved }
-      }).then(({ count, rows: users }) => {
-            res.setHeader("x-total-count", count).json(users); //ліміт
-      }).catch(()=> res.status(500).json({ message: 'Fail to find Users' })); // помилка
+      const { approved = true, start = 0, end = 20 } = req.query;
+      const { limit, offset } = convertStartEndToLimitOffset(start, end);
+      Models.User.findAndCountAll({ limit, offset, approved }).then(({ count, rows: users }) => {
+            res.setHeader("x-total-count", count).json(users);
+      }).catch(()=> res.status(500).json({ message: 'Fail to find Users' }));
       }
 );
 // зробити start і end для users
@@ -21,14 +24,3 @@ export const users = router;
 
 
 
-/*                setTimeout(() => {
-                  const newUsers = users.concat(Array.from({ length: 20 }));
-                  users.push(...newUsers);
-                  limit++;
-                  approved = true;
-            }, 1000);
-            window.addEventListener('scroll', () => {
-                  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-                  if (scrollTop + clientHeight >= scrollHeight - 5 && approved) {
-                        fetchMoreData();
-                  }*/
