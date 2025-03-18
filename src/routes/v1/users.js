c89084c5faff4783f3a2d117cb501c1679ebf48b
AndryGamerTV <asdaartefret@gmail.com>
2025-03-18 23:46:41 +0200

    upd filtration

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { isAprroved, isAuthorized, convertStartEndToLimitOffset} from '../../helpers/index.js';
 import Models from '../../models/index.js';
+import { User } from '../../models/user.js';
 
 const router = express.Router();
 
@@ -9,18 +10,20 @@ router.get('/', isAuthorized, isAprroved, function(req, res){
       const { limit, offset } = convertStartEndToLimitOffset(start, end);
       Models.User.findAndCountAll({ limit, offset, approved }).then(({ count, rows: users }) => {
             res.setHeader("x-total-count", count).json(users);
-      }).catch(()=> res.status(500).json({ message: 'Fail to find Users' }));
+      }).catch(() => res.status(500).json({ message: 'Fail to find Users' }));
       }
 );
-
 router.get('first_name', isAuthorized, isAprroved, function(req, res){
       const { first_name } = req.query;
-      Models.User.findAll({first_name}
-            order: [
-                  ['first_name', 'ASC'],
-            ],
-      )
-)
+      Models.User.findAll({ 
+            where: { first_name },
+            order: [['first_name', 'ASC']]
+      }).then(users => {
+            res.setHeader("x-total-count", count).json(users);
+      }).catch(() => res.status(500).json({ message: 'Fail to find Users' }));
+});
+
+      
 //  first_name, last_name, email, birthday, approved
 // зробити сортування asc/desc, по імені, по email, по даті реєстрації, дні народження
 
